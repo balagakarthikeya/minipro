4846261af9cdeb40179c73251b0d26a49fddfe80
balagakarthikeya <balagakarthikeya@gmail.com>
2025-04-15 17:52:44 +0530

    second commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const documentRoutes = require('./backend/routes/documentRoutes');
 const adminAuthMiddleware = require('./backend/middleware/adminAuth');
 const userAuthMiddleware = require('./backend/middleware/userAuth');
 const path = require('path');
-
+const mongoose = require('mongoose');
 dotenv.config();
 const app = express();
 
@@ -43,6 +43,18 @@ app.get('/', (req, res) => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
+});
+app.get('/health', async (req, res) => {
+    try {
+        const isConnected = mongoose.connection.readyState === 1; // 1 means connected
+        if (isConnected) {
+            res.status(200).json({ success: true, message: 'MongoDB is connected' });
+        } else {
+            res.status(500).json({ success: false, message: 'MongoDB is not connected' });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Error checking MongoDB connection', error: error.message });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
